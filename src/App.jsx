70c8dc9fb49ffd846a6b1f2359f93ffd680aaf01
Tarek Fathy <fathi.tareq.san@gmail.com>
2024-06-15 18:48:53 +0300

    feat: added website name and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './views/Home'
+import "./App.css";
+import Home from "./views/Home";
+import Box from "@mui/material/Box";
+import AboutUs from "./views/AboutUs";
+import AppNavBar from "./components/AppNavBar";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div>
+      <AppNavBar />
+      <Box m={3} pt={1}>
+        <Home />
+        <AboutUs />
+      </Box>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/AppNavBar.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import Grid from "@mui/material/Grid";
+import whiteLogo from "../assests/images/logo_white.png";
+
+export default function AppNavBar() {
+  const pages = ["Home", "About Us"];
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ bgcolor: "#000" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/*<AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />*/}
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item xs={10}>
+              <Box
+                component="img"
+                sx={{ height: 54 }}
+                alt="Logo"
+                src={whiteLogo}
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="NavBarger"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-end"
+            alignItems="center"
+            spacing={2}
+          >
+            <Grid item>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
